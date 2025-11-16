d168b11e0e60d535fc0f5a637c35a56e102d75b5
Pietro Foggia <pietrofoggi03@gmail.com>
2025-11-16 16:11:11 +0100

    finale commit

--- 1_assignement/lu.c
+++ 1_assignement/lu.c
@@ -69,26 +69,28 @@ void kernel_lu(int n, DATA_TYPE A[n][n])
 }
 #endif
 
-#ifdef TASK
-static void kernel_lu(int n, DATA_TYPE POLYBENCH_2D(A, N, N, n, n))
+#ifdef CPU_TASKS
+void kernel_lu(int n, DATA_TYPE A[n][n])
 {
   int k, i, j;
 
     for (k = 0; k < n; ++k)
     {
-
-	  //taskloop funziona meglio di task depend
-	  #pragma omp taskloop simd num_tasks(32) 
-      for (j = k + 1; j < n; ++j) 
-		// #pragma omp task depend(out:A[0:n][0:n])
-        A[k][j] = A[k][j] / A[k][k];
-  
-	  #pragma omp taskloop num_tasks(32) 
-      for (i = k + 1; i < n; ++i) 
+	    //#pragma omp taskloop num_tasks(32) 
+		#pragma omp task depend(out:A[0:n][0:n])
         for (j = k + 1; j < n; ++j) 
-          A[i][j] = A[i][j] - A[i][k] * A[k][j];
-
-	} 
+		  //#pragma omp task depend(out:A[0:n][0:n])
+          A[k][j] = A[k][j] / A[k][k];
+		
+		//#pragma omp taskwait
+	    //#pragma omp taskloop num_tasks(32) 
+		#pragma omp task depend(in:A[0:n][0:n])
+        for (i = k + 1; i < n; ++i) 
+          for (j = k + 1; j < n; ++j) 
+		    //#pragma omp task depend(in:A[0:n][0:n])
+            A[i][j] = A[i][j] - A[i][k] * A[k][j];
+		
+	}
 }
 #endif
 
